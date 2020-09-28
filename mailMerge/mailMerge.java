44d1d0287e8ec07a4b405a49ec147d55108899db
alexandrarusso <alexandrarusso@10.71.28.50>
2020-09-28 17:37:30 -0400

    mailmerge java

---
+++ mailMerge/mailMerge.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class mailMerge {
+
+	public static void main(String[] args) {
+		Scanner Alex = new Scanner(System.in);
+		
+		
+	String James = "Dear James Black";
+		System.out.println("Dear James Black");
+		
+	String Please = "Please confirm that your address is 99 Santa Lane";
+	System.out.println(Please);
+		
+	String Answer = "Your answer is yes";
+		System.out.println(Answer);
+		
+	}
+}
