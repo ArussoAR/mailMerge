cc496ab7ab733ceda1fb47a2d1b472a1500c5d6d
alexandrarusso <alexandrarusso@10.71.28.50>
2020-09-28 21:23:47 -0400

    this is the new one

--- mailMerge/src/mailmerge.java
+++ mailMerge/src/mailmerge.java
@@ -1,7 +1,9 @@
+import java.io.IOException;
 import java.util.Scanner;
-public class mailMerge {
 
-	public static void main(String[] args) {
+public class mailmerge {
+
+	public static void main(String[] args) throws IOException {
 		Scanner Alex = new Scanner(System.in);
 		 
 		//accept first name
@@ -22,28 +24,30 @@ public class mailMerge {
 		String streetName = Alex.next();
 		
 		String text1 = "Dear first last," ;
-		String text2 = "Please confirm that your address is ";
+		String text2 = "Please confirm that your address is house street";
 		String text3 = "Your answer is anything ";
 				
 		
-		text1 = text1.replace("Enter first name", firstName);
-		text1 = text1.replace("Enter last name", lastName);
+		text1 = text1.replace("first", firstName);
+		text1 = text1.replace("last", lastName);
 		
-		text2 = text2.replace("Enter house number", houseNumber);
-		text2 = text2.replace("Enter street name", streetName);
+		text2 = text2.replace("house", houseNumber);
+		text2 = text2.replace("street", streetName);
 		
 		System.out.println(text1);
 		System.out.println(text2);
 		System.out.println("Enter yes or no");
 		
-		String answer = in.next();
-		text3 = text3.replace("anything", "yes or no");
-		System.println(text3);
+		String answer = Alex.next();
+		text3 = text3.replace("anything", answer);
+		System.out.println(text3);
 		
-		in.close();
+		Alex.close();
 		
 		
 		
 
 	}
 }
+
+
